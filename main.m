fdc3f3c78a6c78c80c98dac9370d8f7276f8201f
ruanshutian <1204970556@qq.com>
2022-04-18 09:11:01 +0800

    test

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  test
+//
+//  Created by Aron Ruan on 2022/4/16.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
